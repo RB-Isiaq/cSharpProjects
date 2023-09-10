a54d616c401c1a59d762ced9318e7258d2ff1bac
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-09-10 03:42:33 +0100

    switch statements

--- CodeBlocks/Program.cs
+++ CodeBlocks/Program.cs
@@ -10,8 +10,10 @@ if (flag)
 Console.WriteLine($"Outside of code block: {value}");
 
 // bool flag = true;
-if (flag) Console.WriteLine(flag);string name = "steve";
+if (flag) Console.WriteLine(flag);
 
+
+string name = "steve";
 if (name == "bob")
     Console.WriteLine("Found Bob");
 else if (name == "steve") 
@@ -19,3 +21,47 @@ else if (name == "steve")
 else
     Console.WriteLine("Found Chuck");
 
+// This works
+switch (name)
+{
+    case "bob":
+    Console.WriteLine("Found Bob");
+    break;
+    case "steve":
+    Console.WriteLine("Found Steve");
+    break;
+    default:
+    Console.WriteLine("Found Chuck");
+    break;
+}
+
+switch (name)
+{
+    case "bob" when name == "bob":
+    Console.WriteLine("Found Bob");
+    break;
+    case "steve" when name == "steve":
+    Console.WriteLine("Found Steve");
+    break;
+    default:
+    Console.WriteLine("Found Chuck");
+    break;
+}
+
+
+
+// Variable scope challenge
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42) 
+        found = true;
+        
+    if (found) 
+        Console.WriteLine("Set contains 42");
+}
+
+Console.WriteLine($"Total: {total}");
