06c73ebb2d8fe75c88069b9d261036aecc7cc1b5
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-09-10 06:37:37 +0100

    for loop

---
+++ ForLoop/Program.cs
@@ -0,0 +1,58 @@
+﻿for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+Console.WriteLine("\n------------LINE_BREAKER------------\n");
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\n------------LINE_BREAKER------------\n");
+for (int i = 0; i < 10; i += 3)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("\n------------LINE_BREAKER------------\n");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+Console.WriteLine("\n------------LINE_BREAKER------------\n");
+
+string[] namesReverse = { "Alex", "Eddie", "David", "Michael" };
+for (int i = namesReverse.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(namesReverse[i]);
+}
+Console.WriteLine("\n------------LINE_BREAKER------------\n");
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+//Cannot work, for each iteration does not allow the assignment of values to the iterated variables, it will throw an error
+// Console.WriteLine("\n------------LINE_BREAKER------------\n");
+
+// // string[] names = { "Alex", "Eddie", "David", "Michael" };
+// foreach (var name in names)
+// {
+//     // Can't do this:
+//     if (name == "David") name = "Sammy";
+// }
+
+Console.WriteLine("\n------------LINE_BREAKER------------\n");
+// But this can be done using the for loop below
+
+// string[] names = {"Alex", "Eddie", "David", "Michael"};
+
+for (int i = 0; i < names.Length; i++) 
+    if(names[i] == "David") 
+        names[i] = "Sammy";
+    
+foreach(string name in names)
+    Console.WriteLine(name);
